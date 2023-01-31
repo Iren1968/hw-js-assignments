48b5f32714a06e00bc911b5c875e2fc7a74eba7f
IrinaChizhova <iriska.verbnaya@gmail.com>
2023-01-31 21:02:49 +0300

    Added functions with increment, decrement

--- calculations.js
+++ calculations.js
@@ -18,7 +18,21 @@ function getRestAfterDivision(x, y) {
   return x;
 }
 
+function decreaseBalancePlus(x, y) {
+  x -= y;
+  ++x;
+  return x;
+}
+
+function increaseBalancePlus(x, y) {
+  x += y;
+  --x;
+  return x;
+}
+
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+console.log(decreaseBalancePlus(3000, 700));
+console.log(increaseBalancePlus(3000, 700));
